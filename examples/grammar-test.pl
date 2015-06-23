ce1a34cd834a54736ca838fb42ddd4453d66c8c4
David Mertens <dcmertens.perl@gmail.com>
2015-06-23 08:32:54 -0400

    Fixed grammar example/test, and changed line endings

--- examples/grammar-test.pl
+++ examples/grammar-test.pl
@@ -10,24 +10,26 @@ SEQ numbers => re_sub [0 => '100%'], sub {
 	my $match_info = shift;
 	my $data = $match_info->{data};
 	my $left = $match_info->{left};
+	print "Looking for numbers from $left to $match_info->{right}\n";
 	my $i;
 	for ($i = $left; $i <= $match_info->{right}; $i++) {
 		last if $data->[$i] !~ /^\d+$/;
 	}
 	$i--;
-	return $i - $left;
+	return $i - $left + 1;
 };
 
 SEQ letters => re_sub [0 => '100%'], sub {
 	my $match_info = shift;
 	my $data = $match_info->{data};
 	my $left = $match_info->{left};
+	print "Looking for letters from $left to $match_info->{right}\n";
 	my $i;
 	for ($i = $left; $i <= $match_info->{right}; $i++) {
 		last if $data->[$i] !~ /^[a-zA-Z]+$/;
 	}
 	$i--;
-	return $i - $left;
+	return $i - $left + 1;
 };
 
 package main;
